7af36ac846ea84bab0ebc14a58f5294ae5ce2f9a
Murtuza-Ahmed <murtuza.programmer@gmail.com>
2024-01-31 19:08:47 +0500

    index file add

--- calculator/assets/javascript/app.js
+++ calculator/assets/javascript/app.js
@@ -5,11 +5,19 @@ let string = "";
 let arrayButton = Array.from(buttons);
 arrayButton.forEach((button) => {
     button.addEventListener("click", (element) => {
-        if (element.target.innerText === "=") {
-            console.log(true)
-        } else {
-            console.log(false)
+        if (element.target.innerText === "+") {
+            string = eval(string);
+            displayNumber.value = string;
+        } else if (element.target.innerText === "AC") {
+            string = "";
+            displayNumber.value = string;
+        } else if (element.target.innerText === "DE") {
+            string = string.substring(0, string.length - 1);
+            displayNumber.value = string;
+        }
+        else {
+            string += element.target.innerText;
+            displayNumber.value = string;
         }
     })
 })
-console.log(string)
