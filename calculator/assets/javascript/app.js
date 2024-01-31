de102ffd8c6c731d8da0a8465998f037e2d68348
Murtuza-Ahmed <murtuza.programmer@gmail.com>
2024-01-31 16:27:53 +0500

    paractice

--- calculator/assets/javascript/app.js
+++ calculator/assets/javascript/app.js
@@ -2,8 +2,14 @@
 let displayNumber = document.getElementById("number");
 let buttons = document.querySelectorAll("button");
 buttons.forEach((button) => {
-    button.addEventListener("click", (element) => {
-        console.log(element)
+    button.addEventListener("click", () => {
+        let buttonValue = button.innerText;
+        displayNumber.value += buttonValue;
+        if (buttonValue === "AC") {
+            displayNumber.value = "";
+        } else if (buttonValue === "DEL") {
+            displayNumber.value = displayNumber.value.slice(0, -1);
+        }
     })
 })
-console.log(buttons)
+console.log(displayNumber)
