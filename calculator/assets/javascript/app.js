1c43a2da337902ff1d3907459b9e8476d8ba1339
Murtuza-Ahmed <murtuza.programmer@gmail.com>
2024-01-31 18:24:19 +0500

    condition

--- calculator/assets/javascript/app.js
+++ calculator/assets/javascript/app.js
@@ -1,15 +1,15 @@
 "use strict";
 let displayNumber = document.getElementById("number");
 let buttons = document.querySelectorAll("button");
-buttons.forEach((button) => {
-    button.addEventListener("click", () => {
-        let buttonValue = button.innerText;
-        displayNumber.value += buttonValue;
-        if (buttonValue === "AC") {
-            displayNumber.value = "";
-        } else if (buttonValue === "DEL") {
-            displayNumber.value = displayNumber.value.slice(0, -1);
+let string = "";
+let arrayButton = Array.from(buttons);
+arrayButton.forEach((button) => {
+    button.addEventListener("click", (element) => {
+        if (element.target.innerText === "=") {
+            console.log(true)
+        } else {
+            console.log(false)
         }
     })
 })
-console.log(displayNumber)
+console.log(string)
