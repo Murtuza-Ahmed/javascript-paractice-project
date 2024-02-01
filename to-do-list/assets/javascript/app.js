eee2cae6f82bb92222d3a152c689ea50ddb6a980
Murtuza-Ahmed <murtuza.programmer@gmail.com>
2024-02-01 12:12:17 +0500

    to do paractice

--- to-do-list/assets/javascript/app.js
+++ to-do-list/assets/javascript/app.js
@@ -1,3 +1,32 @@
 const inputElement = document.getElementById("inputBox");
-const toDoList = document.querySelector(".todolist");
+const toDoListOutPut = document.querySelector(".todolist");
 const button = document.querySelector("button");
+button.addEventListener("click", () => {
+    const getInput = inputElement.value.trim();
+    if (getInput === "") {
+        alert("You Must Write SomeThing in your To-Do")
+    } else {
+        // CREATE ELEMENT P
+        const li = document.createElement("li");
+        const p = document.createElement("p");
+        p.innerHTML = getInput;
+        li.appendChild(p);
+        toDoListOutPut.appendChild(li);
+        inputElement.value = "";
+        // CREATE ELEMENT EDIT
+        const editButton = document.createElement("button");
+        editButton.innerText = "Edit";
+        editButton.classList.add("edit", "btn");
+        li.appendChild(editButton);
+        // CREATE ELEMENT DELETE
+        const deleteButton = document.createElement("button");
+        deleteButton.innerText = "Remove";
+        deleteButton.classList.add("delete", "btn");
+        li.appendChild(deleteButton);
+    }
+})
+toDoListOutPut.addEventListener("click", (element) => {
+    if (element.target.innerText === "Remove") {
+        toDoListOutPut.removeChild(element.target.parentElement);
+    }
+})
