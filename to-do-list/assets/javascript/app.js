e874774c19e95296b3952531c03c149dbf2e921b
Murtuza-Ahmed <murtuza.programmer@gmail.com>
2024-02-07 01:59:16 +0500

    paractice

--- to-do-list/assets/javascript/app.js
+++ to-do-list/assets/javascript/app.js
@@ -14,7 +14,7 @@ button.addEventListener("click", () => {
         button.innerHTML = "ADD";
         // INVOKE SAVE LOCAL STORAFE FUNCTION
         saveLocalStorage(inputElement);
-        editLocalTodo(inputElement);
+        // editLocalTodo(inputElement);
         inputElement.value = "";
     } else {
         // CREATE ELEMENT P
@@ -25,7 +25,7 @@ button.addEventListener("click", () => {
         toDoListOutPut.appendChild(li);
         // INVOKE SAVE LOCAL STORAGE FUNCTION
         saveLocalStorage(inputElement);
-        editLocalTodo(inputElement);
+        // editLocalTodo(inputElement);
         inputElement.value = "";
         // CREATE ELEMENT EDIT
         const editButton = document.createElement("button");
@@ -91,13 +91,14 @@ function deleteToDoList(deleteItem) {
     existingToDoArray.splice(toDoArray, 1);
     localStorage.setItem("ToDoKEY", JSON.stringify(existingToDoArray));
 }
-function editLocalTodo(element) {
-    let todos = JSON.parse(localStorage.getItem("ToDoKEY"));
-    let todoIndex = todos.indexOf(element.value);
-    console.log("TODOINDEX", todoIndex)
-    todos.filter((todos, index) => {
-        console.log(todos, index)
-    })
-    localStorage.setItem("ToDoKEY", JSON.stringify(todos));
-}
-editLocalTodo(inputElement);
+// function editLocalTodo(element) {
+//     let todos = JSON.parse(localStorage.getItem("ToDoKEY"));
+//     let todoIndex = todos.indexOf(element.value);
+//     console.log("TODOINDEX", todoIndex)
+//     todos.forEach((todos, index) => {
+//         console.log(todos);
+
+//     })
+//     localStorage.setItem("ToDoKEY", JSON.stringify(todos));
+// }
+// editLocalTodo(inputElement);
