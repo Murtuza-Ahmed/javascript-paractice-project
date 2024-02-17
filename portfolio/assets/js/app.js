ca7f71d45e84f6f38ee14e697fcf57106d7befca
Murtuza-Ahmed <murtuza.programmer@gmail.com>
2024-02-17 12:37:42 +0500

    index file boxicon add

--- portfolio/assets/js/app.js
+++ portfolio/assets/js/app.js
@@ -1,5 +1,4 @@
 window.onscroll = () => {
     let header = document.querySelector(".header");
     header.classList.toggle("sticky", window.scrollY > 100);
-    console.log(header.classList.toggle("sticky", window.scrollY > 100))
 };
