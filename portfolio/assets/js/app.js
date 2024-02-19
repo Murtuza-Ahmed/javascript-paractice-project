a4356682304367855db7005425f6cc0817dada2b
Murtuza-Ahmed <murtuza.programmer@gmail.com>
2024-02-19 11:03:55 +0500

    app.js problem

--- portfolio/assets/js/app.js
+++ portfolio/assets/js/app.js
@@ -2,3 +2,18 @@ window.onscroll = () => {
     let header = document.querySelector(".header");
     header.classList.toggle("sticky", window.scrollY > 100);
 };
+
+// SWIPER SCRIPT FILE
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 1,
+    spaceBetween: 30,
+    loop: true,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+});
