8982d62e23542576acaec7354b5ea0b8ca951187
Murtuza-Ahmed <murtuza.programmer@gmail.com>
2024-02-21 16:21:59 +0500

    api call

--- weather-app/assets/js/app.js
+++ weather-app/assets/js/app.js
@@ -6,3 +6,15 @@ const discripption = document.querySelector(".discription");
 const humidity = document.getElementById("humidity");
 const windSpeed = document.getElementById("wind-speed");
 
+async function checkWeather(city) {
+    const api_key = "3a277e90a743f332915b0088b6568216";
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${api_key}`;
+    const weather_data = await fetch(`${url}`).then(response => response.json());
+    
+
+}
+
+searchButton.onclick = () => {
+    let inputValue = inputBox.value;
+    checkWeather(inputValue);
+};
