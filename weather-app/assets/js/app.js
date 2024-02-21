165ff32d01064b96c9fdec0a0486bd4e1eea157e
Murtuza-Ahmed <murtuza.programmer@gmail.com>
2024-02-21 15:45:04 +0500

    app.js const redy

---
+++ weather-app/assets/js/app.js
@@ -0,0 +1,8 @@
+const inputBox = document.querySelector(".input-box");
+const searchButton = document.querySelector("button");
+const weatherImage = document.querySelector(".sun-cloud");
+const temperature = document.querySelector(".temperature");
+const discripption = document.querySelector(".discription");
+const humidity = document.getElementById("humidity");
+const windSpeed = document.getElementById("wind-speed");
+
