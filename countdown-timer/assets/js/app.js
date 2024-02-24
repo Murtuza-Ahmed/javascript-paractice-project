f4c117fbe71fa4c8092d906ffe9a24d8a844bf1b
Murtuza-Ahmed <murtuza.programmer@gmail.com>
2024-02-24 16:48:43 +0500

    date problem bug

--- countdown-timer/assets/js/app.js
+++ countdown-timer/assets/js/app.js
@@ -1,11 +1,25 @@
-const endDate = "24 May 2024 2:00 PM";
-console.log(new Date(endDate))
+const endDate = "27 Feb 2024 2:00 PM";
 document.getElementById("end-date").innerText = endDate;
+const inputs = document.querySelectorAll("input");
+
 
 const clock = () => {
     const end = new Date(endDate);
     const now = new Date();
-    console.log(end)
-    console.log(now);
+    const diff = (end - now) / 1000;
+    inputs[0].value = Math.floor(diff / 3600 / 24);
+    inputs[1].value = Math.floor(diff / 3600) % 24;
+    inputs[2].value = Math.floor(diff / 60) % 60;
+    inputs[3].value = Math.floor(diff) % 60;
 };
 clock()
+
+
+
+
+
+/**
+ * 1 day = 24 hrs
+ * 1 hrs = 60 min
+ * 60 min = 3600 sec
+ */
